13ec3acf39f30979afb0865a48033b0064c8f24f
Angel Rosario <arosario.us@gmail.com>
2022-04-27 14:31:22 -0400

    basic site setup test

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,28 +1,43 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Card, Image } from 'semantic-ui-react'
 import { SocialIcon } from 'react-social-icons';
 import Button from 'react-bootstrap/Button';
+import Peru from './images/Peru.jpg'
 
 
 function FrontPage() {
 
   return (
-    <div className="front">
+    <div>
 
-      <Header style={{margin:'left'}} as='h1'>Angel Rosario</Header>
+      <Header as='h1'>Angel Rosario</Header>
       <hr></hr>
 
-      <Header as='h2'></Header>
+      <Image src={Peru} className='image' />
+      <p></p>
+      <Card className="center">
+        Who am I?
+          <p></p>I am an aspiring <b>software engineer</b>. 
+          <p></p>I am an avid <b>traveler.</b>
+          <p></p>I am an amateur <b>photographer.</b>
+          <p></p>
+        Want to learn more? Connect with me below. Check out my resume. Contact me.
+      </Card>
+
+
+      <div className="center">
       <SocialIcon className="social" url="https://www.linkedin.com/in/rosarioangel/"/>
       <SocialIcon className="social" url="https://github.com/angelroundtrip"/>
       <SocialIcon className="social" url="https://medium.com/@angelroundtrip"/>
       <SocialIcon className="social" url="https://instagram.com/angelroundtrip"/>
+      {/* <SocialIcon className="social" url="https://twitter.com/angelroundtrip"/> */}
+      </div>
 
-      <p>
+      <p className="center">
         <a href="https://docs.google.com/document/d/1AQAaeIDD4yajFezINGpQzRz1_Xi4EO2pieaLRJ2nzTA"><Button variant="primary" style={{margin:'0.9%'}}>Resume</Button></a>
       </p>
 
-      <p style={{color: 'teal', fontSize: '20px'}}>  
+      <p className="center" style={{color: 'teal', fontSize: '20px'}}>  
         <a href="mailto:angel@angelrosario.dev"> <Button>Contact</Button> </a> 
       </p>
 
