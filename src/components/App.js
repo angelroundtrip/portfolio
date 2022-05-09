08254f516ce695c8c651e95fbd6a1b96e68ec775
Angel Rosario <arosario.us@gmail.com>
2022-05-09 16:35:55 -0400

    minor css updates

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
 
     <Routes>
-      
+
       <Route path="/" element={<FrontPage />}/>
 
     </Routes>
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -10,7 +10,7 @@ function FrontPage() {
   return (
     <div>
 
-      <Header className="left" as='h1'>Angel Rosario</Header>
+      <Header className="center" as='h1'>Angel Rosario</Header>
       <hr></hr>
       <Image src={Peru} className='image' />
 
